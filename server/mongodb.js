87f40901915f13afabc202450d1f3f2fa4937644
FerdiVala <ferdinand.valancogne@edu.devinci.fr>
2022-03-23 19:38:45 +0100

    mongodb

--- server/mongodb.js
+++ server/mongodb.js
@@ -7,15 +7,28 @@ const dedicatedbrand = require('./dedicatedbrand.json');
 const montlimart = require('./montlimart.json');
 const adresseparis = require('./adresseparis.json');
 
-async function Main() {
-    client = MongoClient.connect(MONGODB_URI, { 'useNewUrlParser': true });
-    const db = await client.db(MONGODB_DB_NAME)
-    console.log(`Connected to database ${db.databaseName}`)
-    const collections = await db.collection('products');
-    // await RemoveAll(collections)
-
-    // INSERT PRODUCTS
-    // await InsertAllProducts(collections)
+async function connect() {
+    try {
+        const client = await MongoClient.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+        let connexion_db=client.db(MONGODB_DB_NAME)
+        console.log('Connected to database ')
+        return connexion_db
+    }
+    catch (err) {
+        console.error(`Error connecting to the database. \n${err}`);
+    }
 }
 
-Main()
+connect()
+
+async function insert_products(products) {
+    const db = await connect();
+    const collection = db.collection('products');
+    for (brand_products of products) {
+        const result = collection.insertMany(brand_products);
+    }
+}
+
+// insert_products([montlimart])
+insert_products([adresseparis])
+insert_products([dedicatedbrand])
