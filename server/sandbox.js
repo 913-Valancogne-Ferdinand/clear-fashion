be93890c0710e141abf908c51dc494cbfab86b46
FerdiVala <ferdinand.valancogne@edu.devinci.fr>
2022-03-23 18:10:37 +0100

    scrap

--- server/sandbox.js
+++ server/sandbox.js
@@ -1,11 +1,18 @@
 /* eslint-disable no-console, no-process-exit */
 const dedicatedbrand = require('./sources/dedicatedbrand');
+const adresseparis = require('./sources/adresseparis');
+const montlimart = require('./sources/montlimart');
 
-async function sandbox (eshop = 'https://www.dedicatedbrand.com/en/men/news') {
+// https://adresse.paris/630-toute-la-collection?id_category=630&n=118
+// 
+// https://www.montlimart.com/toute-la-collection.html
+
+
+async function sandbox (eshop = 'https://www.montlimart.com/toute-la-collection.html') {
   try {
     console.log(`🕵️‍♀️  browsing ${eshop} source`);
 
-    const products = await dedicatedbrand.scrape(eshop);
+    const products = await montlimart.scrape(eshop);
 
     console.log(products);
     console.log('done');
---
+++ server/sources/adresseparis.js
@@ -0,0 +1,59 @@
+const fetch = require('node-fetch');
+const cheerio = require('cheerio');
+
+/**
+ * Parse webpage e-shop
+ * @param  {String} data - html response
+ * @return {Array} products
+ */
+const parse = data => {
+    const $ = cheerio.load(data);
+    const released = new Date();
+
+    return $('.product_list .ajax_block_product')
+        .map((i, element) => {
+            const name = $(element)
+                .find('.product-name')
+                .text()
+                .trim()
+                .replace(/\s/g, ' ').split('  ')[0];
+
+            const price = parseInt(
+                $(element)
+                    .find('.price')
+                    .text()
+            );
+            const brand = "Adresse_Paris";
+            var link = $(element)
+                .find('.product-name').attr('href');
+
+            const date = released.toLocaleDateString()
+
+            return {brand, name, price, date};
+        })
+        .get();
+};
+
+/**
+ * Scrape all the products for a given url page
+ * @param  {[type]}  url
+ * @return {Array|null}
+ */
+module.exports.scrape = async url => {
+    try {
+        const response = await fetch(url);
+
+        if (response.ok) {
+            const body = await response.text();
+
+            return parse(body);
+        }
+
+        console.error(response);
+
+        return null;
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
+};
