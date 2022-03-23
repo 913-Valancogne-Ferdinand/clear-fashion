da56b858d5f82f295ab56c4147360e031613e30c
FerdiVala <ferdinand.valancogne@edu.devinci.fr>
2022-03-23 19:29:13 +0100

    mongodb

--- server/mongodb.js
+++ server/mongodb.js
@@ -2,10 +2,20 @@ const { MongoClient } = require('mongodb');
 const MONGODB_URI = 'mongodb+srv://FerdinandValancogne:ferdinand@cluster0.upmid.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const MONGODB_DB_NAME = 'clearfashion';
 
-const client = await MongoClient.connect(MONGODB_URI, {'useNewUrlParser': true});
-const db =  client.db(MONGODB_DB_NAME)
 
-const dedicated_mongodb = require('./dedicated.json');
-const montlimart_mongodb = require('./montlimart.json');
-const adresseparis_mongodb = require('./adresseparis.json');
+const dedicatedbrand = require('./dedicatedbrand.json');
+const montlimart = require('./montlimart.json');
+const adresseparis = require('./adresseparis.json');
 
+async function Main() {
+    client = MongoClient.connect(MONGODB_URI, { 'useNewUrlParser': true });
+    const db = await client.db(MONGODB_DB_NAME)
+    console.log(`Connected to database ${db.databaseName}`)
+    const collections = await db.collection('products');
+    // await RemoveAll(collections)
+
+    // INSERT PRODUCTS
+    // await InsertAllProducts(collections)
+}
+
+Main()
