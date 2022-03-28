b315dd886bfcb969b5ed806cfc89e6510802720c
FerdiVala <ferdinand.valancogne@edu.devinci.fr>
2022-03-28 07:13:05 +0200

    mongodb

--- server/mongodb.js
+++ server/mongodb.js
@@ -2,7 +2,6 @@ const { MongoClient } = require('mongodb');
 const MONGODB_URI = 'mongodb+srv://FerdinandValancogne:ferdinand@cluster0.upmid.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const MONGODB_DB_NAME = 'clearfashion';
 
-
 const dedicatedbrand = require('./dedicatedbrand.json');
 const montlimart = require('./montlimart.json');
 const adresseparis = require('./adresseparis.json');
@@ -15,22 +14,20 @@ async function connect() {
         return db
     }
     catch (err) {
-        console.error(`Error . \n${err}`);
+        console.error(`Error. \n${err}`);
     }
 }
 
-
-
-async function insert_products(products) {
+async function insert(products) {
     const db = await connect();
     const collection = db.collection('products');
-    for (brand_products of products) {
-        const result = collection.insertMany(brand_products);
+    for (brand of products) {
+        collection.insertMany(brand);
     }
 }
 
-insert_products([montlimart])
-insert_products([adresseparis])
-insert_products([dedicatedbrand])
+insert([montlimart])
+insert([adresseparis])
+insert([dedicatedbrand])
 
 module.exports = connect;
