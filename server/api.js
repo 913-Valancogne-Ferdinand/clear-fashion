f5783fe19756ceab1b5f308a0171f238a930797f
FerdiVala <ferdinand.valancogne@edu.devinci.fr>
2022-03-30 16:13:47 +0200

    amélioration final

--- server/api.js
+++ server/api.js
@@ -7,57 +7,47 @@ const PORT = 8092;
 
 const app = express();
 
-
 module.exports = app;
 
 app.use(require('body-parser').json());
 app.use(cors());
 app.use(helmet());
 
-
 app.options('*', cors());
-
 
 //home endpoint
 app.get('/', (request, response) => {
-  response.send({ 'ack': true });
+  response.send({'ack': true });
 });
-
-
-
 
 //endpoint to make a query with parameters
 app.get('/products/search', async (req, res) => {
-
   try {
     res.setHeader('Content-Type', 'text/html')
     res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate')
-    // setting the base parameters
     let size = 12;
     let price = 10000;
     let brand = null;
     let page = 1;
     let query = {};
-    // if parameters are specified in the endpoint, get them
-    if (req.query.size) { size = parseInt(req.query.size); }
-
-    if (req.query.page) {page = parseInt(req.query.page); }
-
-    if (req.query.price) {price = parseFloat(req.query.price); }
-    
+    if (req.query.size) { 
+      size = parseInt(req.query.size);
+    }
+    if (req.query.page) {
+      page = parseInt(req.query.page);
+    }
+    if (req.query.price) {
+      price = parseFloat(req.query.price);
+    }
     if (req.query.brand) {
       brand = req.query.brand;
-      // if a brand is specified putting it in the query
-      query = { brand, "price": { $lte: price } };
-    } query = { "price": { $lte: price } }; //else query wthout brand
-
-    // making the query and sending it to the database
+      query = { brand, "price": { $lte: price }};
+    } 
+    query = { "price": { $lte: price }};
     console.log("getting product from database...")
     const products = await mydb.find(query, size, page);
-    // sending back the products
     if (products) {
       const count = await mydb.getCount();
-      // sending results with the right headers
       res.send({ "success": true, "data": { "result": products, "meta": { "currentPage": page, "pageCount": Math.round(count / size), "pageSize": size, "count": count } } });
     }
   } catch (err) {
@@ -80,7 +70,7 @@ app.get('/product/:id', async (req, res) => {
       res.send({ product });
     }
   } catch (err) {
-    console.log("Error sending the repsonse to the server", err);
+    console.log("Error", err);
     res.send({ ack: "No product found" });
   }
 }); */
